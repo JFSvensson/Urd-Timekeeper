cd514597d8950b4c93c6e19d16e372e2966625cf
Fredrik Svensson <svensson.jf@gmail.com>
2026-01-16 19:12:24 +0100

    Refactor UrdUIServiceFactory to remove ResourceLoader dependency and simplify UrdUIRenderer initialization

--- src/components/urd-timer/UrdUIServiceFactory.ts
+++ src/components/urd-timer/UrdUIServiceFactory.ts
@@ -2,12 +2,10 @@ import { UrdUIService } from './UrdUIService';
 import { UrdTimerService } from './UrdTimerService';
 import { UrdUIRenderer } from './UrdUIRenderer';
 import { UrdUIDOMHandler } from './UrdUIDOMHandler';
-import { ResourceLoader } from '../../services/ResourceLoader';
 
 export class UrdUIServiceFactory {
-  static create(shadowRoot: ShadowRoot, timerService: UrdTimerService, baseUrl: string): UrdUIService {
-    const resourceLoader = new ResourceLoader(baseUrl);
-    const uiRenderer = new UrdUIRenderer(shadowRoot, resourceLoader);
+  static create(shadowRoot: ShadowRoot, timerService: UrdTimerService): UrdUIService {
+    const uiRenderer = new UrdUIRenderer(shadowRoot);
     const domHandler = new UrdUIDOMHandler(shadowRoot, timerService);
     return new UrdUIService(shadowRoot, timerService, uiRenderer, domHandler);
   }
