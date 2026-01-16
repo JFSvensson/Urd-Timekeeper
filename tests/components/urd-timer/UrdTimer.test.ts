58085c8421e7cdb510ce18946bdbfd99e6c59d2c
Fredrik Svensson <svensson.jf@gmail.com>
2026-01-16 20:22:25 +0100

    refactor: update UrdTimer tests to mock UrdSettingsManager for improved settings management

--- tests/components/urd-timer/UrdTimer.test.ts
+++ tests/components/urd-timer/UrdTimer.test.ts
@@ -1,12 +1,14 @@
 import { UrdTimer } from '../../../src/components/urd-timer/UrdTimer';
 import { UrdTimerService } from '../../../src/components/urd-timer/UrdTimerService';
 import { UrdUIService } from '../../../src/components/urd-timer/UrdUIService';
+import { UrdSettingsManager } from '../../../src/components/urd-timer/UrdSettingsManager';
 import { BrowserStorageService } from '../../../src/services/BrowserStorageService';
 import { WebPageMessageService } from '../../../src/services/WebPageMessageService';
 
 jest.mock('../../../src/components/urd-timer/UrdTimerService');
 jest.mock('../../../src/components/urd-timer/UrdUIRenderer');
 jest.mock('../../../src/components/urd-timer/UrdUIDOMHandler');
+jest.mock('../../../src/components/urd-timer/UrdSettingsManager');
 
 jest.mock('../../../src/components/urd-timer/UrdUIService', () => {
   return {
@@ -27,16 +29,19 @@ describe('UrdTimer', () => {
   let mockUIService: jest.Mocked<UrdUIService>;
   let mockStorageService: jest.Mocked<BrowserStorageService>;
   let mockMessageService: jest.Mocked<WebPageMessageService>;
+  let mockSettingsManager: jest.Mocked<UrdSettingsManager>;
 
   beforeEach(() => {
     mockStorageService = jest.mocked(new BrowserStorageService());
     mockMessageService = jest.mocked(new WebPageMessageService());
+    mockSettingsManager = jest.mocked(new UrdSettingsManager(mockStorageService));
   
-    mockTimerService = jest.mocked(new UrdTimerService(mockStorageService, mockMessageService));
+    mockTimerService = jest.mocked(new UrdTimerService(mockSettingsManager, mockMessageService));
     mockUIService = jest.mocked(new UrdUIService(null as any, mockTimerService, null as any, null as any));
   
     (UrdTimerService as jest.MockedClass<typeof UrdTimerService>).mockImplementation(() => mockTimerService);
     (UrdUIService as jest.MockedClass<typeof UrdUIService>).mockImplementation(() => mockUIService);
+    (UrdSettingsManager as jest.MockedClass<typeof UrdSettingsManager>).mockImplementation(() => mockSettingsManager);
   
     timer = new UrdTimer(mockStorageService, mockMessageService);
   });
